ef5e18d4b51e0672abeaea5b53a5de082225c404
aratod <nil.the.human@gmail.com>
2021-01-24 13:25:26 +0100

    Complete test coverage of verify_english.py

--- tests/test_verify_english.py
+++ tests/test_verify_english.py
@@ -13,5 +13,17 @@ def test_non_english_characters():
 def test_prevalence_of_english_characters():
     assert auscultor.verify_english.prevalence_of_english_characters(UDHR_ARTICLE_1) == 1
 
+def test_strip_common_non_verbs():
+    words = get_words(UDHR_ARTICLE_1)
+    assert len(auscultor.verify_english.strip_common_non_verbs(words)) == 20
+
+def test_strip_common_words():
+    words = get_words(UDHR_ARTICLE_1)
+    assert len(auscultor.verify_english.strip_common_words(words)) == 18
+
+def test_prevalence_of_common_words():
+    words = get_words(UDHR_ARTICLE_1)
+    assert auscultor.verify_english.prevalence_of_common_words(words) >= 0.4
+
 def test_is_in_english():
     assert auscultor.verify_english.is_in_english(UDHR_ARTICLE_1) == True
