78dfed9124d3193232f73c3aeb465e05cfd35be7
aratod <nil.the.human@gmail.com>
2021-01-24 13:13:45 +0100

    Add tokenize.py with basic tokenizer functions

---
+++ auscultor/tokenize.py
@@ -0,0 +1,11 @@
+# Helper functions for breaking up text into sentences and words
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+get_sentences = sent_tokenize
+
+def get_words(string):
+    tokens = word_tokenize(string)
+    words = list(filter(lambda x: any(map(str.isalnum, x)), tokens))
+    words_lower = list(map(lambda x: x.lower(), words))
+    return words_lower
