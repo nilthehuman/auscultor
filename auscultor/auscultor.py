52b67cbf094ae500aeb5044c217816059cfa087f
aratod <nil.the.human@gmail.com>
2021-01-24 13:28:55 +0100

    Add a __main__ entry point to auscultor.py

--- auscultor/auscultor.py
+++ auscultor/auscultor.py
@@ -1,3 +1,14 @@
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("url", help="the webpage you wish to download and analyze")
+#parser.parse_args()
+
 def hello():
     """Say hello."""
     print("hello world")
+
+def main():
+    hello()
+
+if __name__ == '__main__':
+    main()
